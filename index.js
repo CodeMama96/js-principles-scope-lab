c7dc91a78ddedd365b4c48400e2d1481198bd1cf
Alicia Weenum <weenum.alicia@gmail.com>
2021-01-29 10:28:38 -0500

    commit tree 00378754176231765294de702b680b750bf2936b

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob"; //global variable 
+//retains a reference to the outer scope in which it was declared.
+//What matters for the scope chain is where the function is declared — not where it is invoked.
+const leastFavoriteCustomer = 'still bob';
+var bestCustomer;
+
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase(); //uppercase this
+    //need to return BOB
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+   bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+     leastFavoriteCustomer = 'is it bob';
+}
